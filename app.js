fa9ce60a02f0a1f50aab8622b218585121b0ee47
DaniilBelousov <chetrik10@gmail.com>
2022-11-10 00:06:59 +0300

    feat: init validation

--- app.js
+++ app.js
@@ -2,26 +2,32 @@
 
 const path = require('path');
 const AutoLoad = require('@fastify/autoload');
+const { addSchemas } = require('./lib/validation');
 
 // Pass --options via CLI arguments in command to enable these options.
 module.exports.options = {};
 
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
+module.exports = async function (app, opts) {
+  await addSchemas(app);
+  app.setErrorHandler(function (error, request, reply) {
+    // Log error
+    this.log.error(error);
+    // Send error response
+    reply.status(500).send({ message: error });
+  });
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
+  app.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
-  fastify.register(AutoLoad, {
+  app.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
   });
--- plugins/sensible.js
+++ plugins/sensible.js
@@ -7,8 +7,8 @@ const fp = require('fastify-plugin');
  *
  * @see https://github.com/fastify/fastify-sensible
  */
-module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/sensible'), {
+module.exports = fp(async function (app, opts) {
+  app.register(require('@fastify/sensible'), {
     errorHandler: false
   });
 });
