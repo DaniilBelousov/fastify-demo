75e15ccebd878630aacb6f411da9b3966dc7b898
DaniilBelousov <chetrik10@gmail.com>
2022-12-10 04:27:11 +0300

    fix: fixed tests setup

--- jest-setup.js
+++ jest-setup.js
@@ -1,8 +1,11 @@
-const { build, onTestEnd } = require('./lib/test-env');
+const { build, clearTestData } = require('./lib/test-env');
 
 module.exports = async () => {
   const app = await build();
-  app.addHook('onClose', async app => onTestEnd(app));
-  app.ready();
+  app.addHook('onClose', async ({ knex }) => {
+    await knex.destroy();
+  });
   globalThis.app = app;
+  await clearTestData(globalThis.app);
+  globalThis.app.ready();
 };
--- jest-teardown.js
+++ jest-teardown.js
@@ -1,3 +1,6 @@
+const { clearTestData } = require('./lib/test-env');
+
 module.exports = async () => {
+  await clearTestData(globalThis.app, { destroy: true });
   await globalThis.app.close();
 };
